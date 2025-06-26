a7a0f51a46a04857b09934a3d1259e42fc6ff03a
Fons van der Plas <fonsvdplas@gmail.com>
2025-06-26 09:39:08 +0200

    Test `One.Two.@asdf`

--- test/ReactiveNode.jl
+++ test/ReactiveNode.jl
@@ -1,17 +1,17 @@
 @testset "ReactiveNode" begin
+    rn = compute_reactive_node(:(Pack))
+    @test rn.references == Set([:Pack])
+
     rn = ExpressionExplorer.compute_reactive_node(quote
         () -> Date
     end)
     @test :Date ∈ rn.references
 
-    rn = compute_reactive_node(:(Pack))
-    @test rn.references == Set([:Pack])
-
     rn = compute_reactive_node(:(@asdf a[1,k[j]] := log(x[i]/y[j])))
     @test rn.references == Set([Symbol("@asdf")])
     @test rn.macrocalls == Set([Symbol("@asdf")])
 
-    rn = compute_reactive_node(:(Pack.@asdf a[1,k[j]] := log(x[i]/y[j])))
-    @test rn.references == Set([:Pack, Symbol("Pack.@asdf")])
-    @test rn.macrocalls == Set([Symbol("Pack.@asdf")])
+    rn = compute_reactive_node(:(One.Two.@asdf a[1,k[j]] := log(x[i]/y[j])))
+    @test rn.references == Set([:One, Symbol("One.Two.@asdf")])
+    @test rn.macrocalls == Set([Symbol("One.Two.@asdf")])
 end
