422b29e70cbae15053988e4cd27a88ad3483e90d
Jürgen Fuhrmann <juergen-fuhrmann@web.de>
2026-02-22 20:57:02 +0100

    Satisfiy check_all_explicit_imports_are_public in PlutoDependencyExplorer

--- src/ExpressionExplorer.jl
+++ src/ExpressionExplorer.jl
@@ -11,7 +11,14 @@ export compute_symbols_state,
     SymbolsState,
     FunctionName,
     FunctionNameSignaturePair,
-    compute_usings_imports
+    compute_usings_imports,
+    UsingsImports,
+    ScopeState,
+    external_package_names,
+    is_function_assignment,
+    split_funcname
+
+
 
 include("./explore.jl")
 include("./UsingsImports.jl")
